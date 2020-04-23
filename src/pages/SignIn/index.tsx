6bce0cee98ade2effba47d666a1ca948a67ab2bf
Matheus Santos <matheusmendes1@hotmail.com>
2020-04-23 20:17:37 -0300

    LogIn screen -v1

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Do your logon</h1>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Password"
+        />
+
+        <Button type="submit">Enter</Button>
+
+        <a href="forgot">Forgot my password</a>
+      </form>
+
+      <a href="create">
+        <FiLogIn />
+        Create Account
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
