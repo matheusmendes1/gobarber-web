1ed252432753cd093d125adfdf1f4b2405b13bbb
Matheus Santos <matheusmendes1@hotmail.com>
2020-05-28 00:23:31 -0300

    Perfil do usuário

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,197 @@
+import React, { useCallback, useRef, ChangeEvent } from 'react';
+import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+
+import * as Yup from 'yup';
+import { useHistory, Link } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/toast';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import { Container, Content, AvatarInput } from './styles';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { useAuth } from '../../hooks/auth';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  old_password: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const history = useHistory();
+  const { addToast } = useToast();
+
+  const { user, updateUser } = useAuth();
+
+  const handleSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Name is required'),
+          email: Yup.string()
+            .required('Email is required')
+            .email('Email is invalid'),
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          old_password: Yup.string(),
+          password: Yup.string().when('old_password', {
+            is: (val) => !!val.length,
+            then: Yup.string().required(),
+            otherwise: Yup.string(),
+          }),
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          password_confirmation: Yup.string()
+            .when('old_password', {
+              is: (val) => !!val.length,
+              then: Yup.string().required(),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const {
+          name,
+          email,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          old_password,
+          password,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          password_confirmation,
+        } = data;
+
+        const formData = {
+          name,
+          email,
+          ...// eslint-disable-next-line @typescript-eslint/camelcase
+          (old_password
+            ? {
+                // eslint-disable-next-line @typescript-eslint/camelcase
+                old_password,
+                password,
+                // eslint-disable-next-line @typescript-eslint/camelcase
+                password_confirmation,
+              }
+            : {}),
+        };
+
+        const response = await api.put('/profile', formData);
+
+        updateUser(response.data);
+
+        history.push('/dashboard');
+
+        addToast({
+          type: 'success',
+          title: 'Profile updated',
+          description: 'Your information was updated with success!',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Error on update',
+          description:
+            'An error occurred while updating your information, please try again.',
+        });
+      }
+    },
+    [addToast, history, updateUser],
+  );
+
+  const handleAvatarChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const data = new FormData();
+        data.append('avatar', e.target.files[0]);
+
+        api.patch('/users/avatar', data).then((response) => {
+          updateUser(response.data);
+
+          addToast({
+            type: 'success',
+            title: 'Avatar atualizado!',
+          });
+        });
+      }
+    },
+    [addToast, updateUser],
+  );
+
+  return (
+    <Container>
+      <header>
+        <div>
+          <Link to="/dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
+      <Content>
+        <Form
+          ref={formRef}
+          initialData={{ name: user.name, email: user.email }}
+          onSubmit={handleSubmit}
+        >
+          <AvatarInput>
+            <img src={user.avatar_url} alt={user.name} />
+            <label htmlFor="avatar">
+              <FiCamera />
+              <input type="file" id="avatar" onChange={handleAvatarChange} />
+            </label>
+          </AvatarInput>
+
+          <h1>My profile</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Name" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            containerStyle={{ marginTop: 30 }}
+            name="old_password"
+            icon={FiLock}
+            type="password"
+            placeholder="Actual password"
+          />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="New password"
+          />
+
+          <Input
+            name="password_confirmation"
+            icon={FiLock}
+            type="password"
+            placeholder="Confirm password"
+          />
+
+          <Button type="submit">Confirm changes</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
